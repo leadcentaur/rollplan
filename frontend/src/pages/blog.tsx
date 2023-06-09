4cb60843e91dc92c401c42e1516e483636b910e9
Brenden <brenden111@hotmail.com>
2023-06-09 01:20:38 -0400

    pushing changes

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function BlogPage() {
   return (
     <div>
-      Welcome home!
+      Blog page
     </div>
   )
 }
