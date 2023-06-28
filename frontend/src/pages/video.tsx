f78e94daad88abe00ded78f87ab91733e5ed1cdd
leadcentaur <brenden111@hotmail.com>
2023-06-27 23:18:12 -0400

    added videos asset

---
+++ frontend/src/pages/video.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import loginVideo from "../assets/videos/loginMatchA.mp4";
+
+export default function Videopage(){
+     return (
+        <video src={loginVideo}
+     );
+}
