a158d42e55e2f9c2d1442bc7930f71d3d1caf0d0
Brenden <brenden111@hotmail.com>
2023-08-02 00:37:15 -0400

    added comment

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+//function to destroy all active sessions
 export async function destroyAllActiveSessionsForUser(userId: string) {
     const regexp = new RegExp("^" + userId);
     mongoose.connection.db.collection("sessions").deleteMany({_id: regexp});
